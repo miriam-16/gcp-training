90a816560aaf8e493e17a6d4086ccb31098c47b2
st203868 <miriam.lamari2@gmail.com>
2023-05-24 11:40:46 +0200

    Versione completa

--- modules/vm_instance_module/main.tf
+++ modules/vm_instance_module/main.tf
@@ -1,36 +1,6 @@
-// versione singola
-/*resource "google_compute_instance" "vm_instance" {
-    for_each = var.vm_instances
-        name            = lookup(each.value, "name",each.key)
-        machine_type    = lookup(each.value, "machine_type","e2-micro")
-        #optional
-        zone            = lookup(each.value, "zone",null)
-        tags            = lookup(each.value, "tags",null)
-        boot_disk {
-            initialize_params {
-                image   = lookup(each.value.boot_disk.inizialize_params, "image", "debian-cloud/debian-11")
-            }
-        }
-
-        network_interface {
-            subnetwork          = each.value.network_interface.subnetwork
-            network             =  each.value.network_interface.network
-            subnetwork_project  = each.value.network_interface.subnetwork_project
-            access_config{
-                nat_ip                  = lookup(each.value.network_interface.access_config, "nat_ip", null)
-                public_ptr_domain_name  = lookup(each.value.network_interface.access_config, "public_ptr_domain_name", null)
-                network_tier            = lookup(each.value.network_interface.access_config, "network_tier", null)
-            }
-        }
-} */
-
-
-
-
-
 resource "google_compute_instance" "vm_instance" {
     for_each = var.vm_instances
-        name            = lookup(each.value, "name",each.key)
+        name            = lookup(each.value, "name", each.key)
         machine_type    = lookup(each.value, "machine_type","e2-micro")
         #optional
         zone            = lookup(each.value, "zone",null)
@@ -41,8 +11,9 @@ resource "google_compute_instance" "vm_instance" {
             }
         }
 
-        dynamic network_interface {
-            for_each = {for key, value in each.value.network_interface : key => value if key == "network_interface"}
+
+        dynamic "network_interface" {
+            for_each = {for key, value in each.value.network_interfaces : key => value}
             content{
                 subnetwork          = network_interface.value.subnetwork
                 network             = network_interface.value.network
@@ -60,7 +31,7 @@ resource "google_compute_instance" "vm_instance" {
 resource "google_compute_disk" "default" {
     for_each = {for k in flatten([
         for key, vm in var.vm_instances : [
-            for key_disk, disk in vm.attached_disk : {
+            for key_disk, disk in vm.attached_disks : {
                 vm_key      = key
                 disk_key    = key_disk
                 disk        = disk
@@ -72,7 +43,6 @@ resource "google_compute_disk" "default" {
     type  =  lookup(each.value.disk, "type", "pd-ssd")
     zone  = lookup(each.value.disk, "zone","eu-west8-a")
     image = lookup(each.value.disk, "image", "debian-11-bullseye-v20220719")
-    #TODO : è corretto così?
     labels = lookup(each.value, "labels", null)
     physical_block_size_bytes = each.value.disk.physical_block_size_bytes
 }
@@ -80,7 +50,7 @@ resource "google_compute_disk" "default" {
 resource "google_compute_attached_disk" "default" {
     for_each = {for k in flatten([
         for key, vm in var.vm_instances : [
-            for key_disk, disk in vm.attached_disk : {
+            for key_disk, disk in vm.attached_disks : {
                 vm_key      = key
                 disk_key    = key_disk
                 disk        = disk
